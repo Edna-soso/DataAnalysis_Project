949f2cb3cbb6a537e12a429eafed27d964379906
Edna-soso <72458113+Edna-soso@users.noreply.github.com>
2021-05-18 02:49:34 +0700

    Split class

---
+++ Data_Prepro/Data_Precessing.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import files
+
+class PreProcessing:
+    def __init__(self):
+        super.__init__()
+        
+    def Del_Nul(self,data):
+        data['Date']=[item[0] for item in data['InvoiceDate'].str.split()]
+        data['Time']=[item[1] for item in data['InvoiceDate'].str.split()]
+        data['Hour']=[item[0] for item in data['Time'].str.split(':')]
+        data['Month']=[item[0] for item in data['Date'].str.split('/')]
+        data['Year']=[item[2] for item in data['Date'].str.split('/')]
+        data['TotalCost']=data['Quantity']*data['UnitPrice']
+    
+    def Dow_Data(self,data):
+        data.to_csv('filename.csv') 
+        files.download('filename.csv')
