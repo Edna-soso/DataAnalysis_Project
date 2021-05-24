0725646c53eb966928a567c51b15119c94353ddd
phuonghpd11 <72453423+phuongha47@users.noreply.github.com>
2021-05-24 22:19:22 +0700

    Update Data_Precessing.py
    true_data class

--- Data_Prepro/Data_Precessing.py
+++ Data_Prepro/Data_Precessing.py
@@ -2,17 +2,21 @@ import numpy as np
 import pandas as pd
 import files
 
-class PreProcessing:
+class data_preprocessing:
     def __init__(self):
         super.__init__()
         
-    def Insert_Columns(self,data):
-        data['Date']=[item[0] for item in data['InvoiceDate'].str.split()]
-        data['Time']=[item[1] for item in data['InvoiceDate'].str.split()]
-        data['Hour']=[item[0] for item in data['Time'].str.split(':')]
-        data['Month']=[item[0] for item in data['Date'].str.split('/')]
-        data['Year']=[item[2] for item in data['Date'].str.split('/')]
-        data['TotalCost']=data['Quantity']*data['UnitPrice']
+    def True_Data(self):
+        data.isna().sum()
+        self.data = data[pd.notnull(data['CustomerID'])]
+        self.data = data.query("Quantity > 0")  
+        
+    def Add_Colunm(self):
+         data['Date']=[item[0] for item in data['InvoiceDate'].str.split()]
+         data['Time']=[item[1] for item in data['InvoiceDate'].str.split()]
+         data['Hour']=[item[0] for item in data['Time'].str.split(':')]
+         data['Month']=[item[0] for item in data['Date'].str.split('/')]
+         data['TotalCost']=data['Quantity']*data['UnitPrice']
     
     def Dow_Data(self,data):
         data.to_csv('filename.csv') 
